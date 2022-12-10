9aa47ebedd4f8c4f5700eae971a199cf8e497f55
cooper-grc <cfg5bkz@virginia.edu>
2022-12-10 11:24:05 -0500

    usb mic start

--- software/pianoputer/pianoputer.py
+++ software/pianoputer/pianoputer.py
@@ -421,6 +421,44 @@ def process_args(parser: argparse.ArgumentParser, args: Optional[List]) -> Tuple
         keyboard_path = os.path.join(CURRENT_WORKING_DIR, keyboard_path)
     return wav_path, keyboard_path, args.clear_cache
 
+def record_sound():
+    form_1 = pyaudio.paInt16 # 16-bit resolution
+    chans = 1 # 1 channel
+    samp_rate = 44100 # 44.1kHz sampling rate
+    chunk = 4096 # 2^12 samples for buffer
+    record_secs = 3 # seconds to record
+    dev_index = 2 # device index found by p.get_device_info_by_index(ii)
+    wav_output_filename = 'test1.wav' # name of .wav file
+
+    audio = pyaudio.PyAudio() # create pyaudio instantiation
+
+    # create pyaudio stream
+    stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
+                        input_device_index = dev_index,input = True, \
+                        frames_per_buffer=chunk)
+    print("recording")
+    frames = []
+
+    # loop through stream and append audio chunks to frame array
+    for ii in range(0,int((samp_rate/chunk)*record_secs)):
+        data = stream.read(chunk)
+        frames.append(data)
+
+    print("finished recording")
+
+    # stop the stream, close it, and terminate the pyaudio instantiation
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    # save the audio frames as .wav file
+    wavefile = wave.open(wav_output_filename,'wb')
+    wavefile.setnchannels(chans)
+    wavefile.setsampwidth(audio.get_sample_size(form_1))
+    wavefile.setframerate(samp_rate)
+    wavefile.writeframes(b''.join(frames))
+    wavefile.close()
+
 
 def play_pianoputer(args: Optional[List[str]] = None):
     """Organize the data and trigger the playing of the samplisizer.
@@ -429,6 +467,7 @@ def play_pianoputer(args: Optional[List[str]] = None):
         args -- list of arguments 
     """
     # Information variables from parser
+    record_sound()
     parser = get_parser()
     wav_path, keyboard_path, clear_cache = process_args(parser, args)
     # Pull audio data from wave file
