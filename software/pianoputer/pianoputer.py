10d5e3f0e291825859943fd4fc800f614804c8f0
cooper-grc <cfg5bkz@virginia.edu>
2022-12-12 11:20:10 -0500

    fix

--- software/pianoputer/pianoputer.py
+++ software/pianoputer/pianoputer.py
@@ -646,7 +646,7 @@ def play_pianoputer(args: Optional[List[str]] = None):
 def handle_midi_input(event, sounds=None):
     message, deltatime = event
     print("len " + str(len(sounds)))
-    print("i " + str(i))
+    print("i " + str(index))
     print(f'message: {message}, deltatime: {deltatime}')
     index = message[1] - 69 + 23
     if message[2] >0:
